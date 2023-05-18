e86fe9917b9f830b928efc94f82c538134a1255b
Nguyen Do Cuong <34064401+docuong0912@users.noreply.github.com>
2023-05-18 23:35:44 +0700

    Update DestinationController.java

--- space-tourism-website-main/src/main/java/com/frontendmentor/spacetourismwebsitemain/destination/DestinationController.java
+++ space-tourism-website-main/src/main/java/com/frontendmentor/spacetourismwebsitemain/destination/DestinationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "api/v1/destination")
-@CrossOrigin(origins={"http://localhost:3000","https://space-tourism-website-main-cszn.vercel.app"})
+@CrossOrigin("https://space-tourism-website-main-cszn.vercel.app")
 public class DestinationController {
     
     private final DestinationService dService;
